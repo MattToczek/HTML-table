d8f1b1cdd86111cc4fcf31f9b7fdb19e86d51065
Matt. Toczek <toczek.matt@gmail.com>
2019-08-28 12:17:28 +0100

    adding button using javascript

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 
 let linkArray = document.getElementsByClassName('link');
-
-console.log(linkArray);
+const filterButton = document.createElement('button');
+const wrapperDiv = document.querySelector(".wrapper");
 
 for (var i = 0; i < linkArray.length; i++) {
   let mailToLink = linkArray[i].innerHTML;
   linkArray[i].href = "mailto:" + mailToLink;
 }
+
+filterButton.innerHTML = "Filter";
+wrapperDiv.appendChild(filterButton);
